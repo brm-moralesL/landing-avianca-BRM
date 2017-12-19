17ab67ce706006f8cdad65c01ad037f4662b6a06
leonardo morales <leonardo.morales@brm.com.co>
2017-12-19 10:30:30 -0500

    Landing Avianca BRM

---
+++ publication/js/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//Keywords
+var demo = 'variable';
+var fijo = 'const';
+
+//arrow functions
+
+demo = function demo() {
+
+	for (var x = 0; x < 5; x++) {
+		console.log(x);
+	}
+};
+
+jQuery(document).ready(function ($) {
+	console.log('hola');
+});
+//# sourceMappingURL=../maps/test.js.map
