da58cd1259d2dfec52b7a1082542790e96a27104
leonardo morales <leonardo.morales@brm.com.co>
2017-12-19 16:41:24 -0500

    Landgin BRM

--- publication/js/test.js
+++ publication/js/test.js
@@ -1,19 +1,38 @@
-'use strict';
-
-//Keywords
-var demo = 'variable';
-var fijo = 'const';
-
-//arrow functions
-
-demo = function demo() {
-
-	for (var x = 0; x < 5; x++) {
-		console.log(x);
-	}
-};
+"use strict";
 
 jQuery(document).ready(function ($) {
-	console.log('hola');
+	var limit = 100,
+	    // Max number of stars
+	screenWidth = $(window).width();
+
+	body = document.getElementById('sparks');
+
+	loop = {
+		//initilizeing
+		start: function start() {
+			for (var i = 0; i <= limit; i++) {
+				var spark = this.newSpark();
+				spark.style.top = this.rand() * 100 + "%";
+				spark.style.left = this.rand() * 100 + "%";
+				spark.style.webkitAnimationDelay = this.rand() + "s";
+				spark.style.mozAnimationDelay = this.rand() + "s";
+				body.appendChild(spark);
+			};
+		},
+		//to get random number
+		rand: function rand() {
+			return Math.random();
+		},
+		//createing html dom
+		newSpark: function newSpark() {
+			var d = document.createElement('div');
+			d.innerHTML = '<div class="spark"></div>';
+			return d.firstChild;
+		}
+	};
+	/*initialize sparks animation for desktop only*/
+	if (screenWidth > 768) {
+		loop.start();
+	}
 });
 //# sourceMappingURL=../maps/test.js.map
