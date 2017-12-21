01f87ded2ac7fb58e7097157831e0db401b236e3
brm-moralesL <leonardo.morales@brm.com.co>
2017-12-21 13:24:48 -0500

    video actualizado y legales

---
+++ src/es6/efectos.js
@@ -0,0 +1,35 @@
+const limit=100, // Max number of stars
+
+body=document.getElementById('sparks');
+
+var loop={
+	//initilizeing
+	 start:function(){
+	 for (var i=0; i <= limit; i++) {
+	        var spark=this.newSpark();
+	        spark.style.top=this.rand()*100+"%";
+	        spark.style.left=this.rand()*100+"%";
+	        spark.style.webkitAnimationDelay=this.rand()+"s";
+	        spark.style.mozAnimationDelay=this.rand()+"s";
+	        body.appendChild(spark);
+	     };
+	   },
+	    //to get random number
+	   rand:function(){
+	       return Math.random();
+	   },
+	    //createing html dom
+	    newSpark:function(){
+	       var d = document.createElement('div');
+	         d.innerHTML = '<div class="spark"></div>';
+	         return d.firstChild;
+	       },
+	};
+
+jQuery(document).ready(function() {
+	var screenWidth = jQuery(window).width();
+	/*initialize sparks animation for desktop only*/
+	 if( screenWidth > 768 ){
+	   loop.start();
+	 }
+});
